456f1e79919a54700d36dec2ef2cbffc12db9a04
callmechelsea <133617421+callmechelsea@users.noreply.github.com>
2023-08-08 19:33:21 -0700

    added routes. can't get to work.

---
+++ server.js
@@ -0,0 +1,21 @@
+// DEPENDENCIES
+const express = require("express");
+
+// EXPRESS CONFIGURATION
+const app = express();
+
+let PORT = process.env.PORT || 8080;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname+"/public"))
+
+// ROUTES 
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
+
+// LISTENER
+app.listen(PORT, function() {
+    console.log("App is listening on PORT: " + PORT);
+});
+
